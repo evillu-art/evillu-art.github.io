57d5695d8bd73e4c2b9486269c58e9ed4904f6fe
Daniel Szogyenyi <szogyenyid@Daniels-MacBook-Air.local>
2024-04-10 13:29:01 +0200

    feat: add dummy pages

---
+++ reactize/src/Components/Home.jsx
@@ -0,0 +1,8 @@
+// Home.js
+import React from 'react';
+
+const Home = () => {
+  return <h1>Welcome to Home</h1>;
+};
+
+export default Home;
---
+++ reactize/src/Components/SilentBook.jsx
@@ -0,0 +1,8 @@
+// SilentBook.js
+import React from 'react';
+
+const SilentBook = () => {
+  return <h1>Welcome to Silent Book</h1>;
+};
+
+export default SilentBook;
