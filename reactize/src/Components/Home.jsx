8ce2f4c77a564dd1b1e3bbb11bff0ffe97fcaad9
Daniel Szogyenyi <szogyenyid@Daniels-MacBook-Air.local>
2024-04-11 14:48:57 +0200

    start making it SPA

--- reactize/src/Components/Home.jsx
+++
@@ -1,8 +0,0 @@
-// Home.js
-import React from 'react';
-
-const Home = () => {
-  return <h1>Welcome to Home</h1>;
-};
-
-export default Home;
--- reactize/src/Components/SilentBook.jsx
+++
@@ -1,8 +0,0 @@
-// SilentBook.js
-import React from 'react';
-
-const SilentBook = () => {
-  return <h1>Welcome to Silent Book</h1>;
-};
-
-export default SilentBook;
