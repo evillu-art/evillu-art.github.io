8f89179496efa7b6335ac9a9b893262985876e6a
Daniel Szogyenyi <szogyenyid@Daniels-MacBook-Air.local>
2024-04-11 18:52:56 +0200

    refactor: ES6 compatible navbar.js

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,49 +1,49 @@
-function openNav() {
+const openNav = () => {
     document.getElementById("navbar").style.width = "250px";
     const hamburger = document.getElementById("hamburger");
-    hamburger.removeEventListener("click", openNav)
+    hamburger.removeEventListener("click", openNav);
     hamburger.addEventListener("click", closeNav);
   }
 
-function closeNav() {
+const closeNav = () => {
     document.getElementById("navbar").style.width = "0";
     const hamburger = document.getElementById("hamburger");
-    hamburger.removeEventListener("click", closeNav)
+    hamburger.removeEventListener("click", closeNav);
     hamburger.addEventListener("click", openNav);
 }
 
-function clickLink(id) {
+const clickLink = (id) => {
     if (isActive(id)) {
         return;
     }
 
     switch (id) {
         case "ar-exp-nav":
-        loadPage("ar")
+        loadPage("ar");
         break;
     case "silent-nav":
-        removeAr()
-        loadPage("silent-book")
+        removeAr();
+        loadPage("silent-book");
         break;
     case "alszol-nav":
-        removeAr()
-        loadPage("te-jol-alszol")
+        removeAr();
+        loadPage("te-jol-alszol");
         break;
     }
-    makeActive(id)
+    makeActive(id);
 }
 
-function isActive(id) {
+const isActive = (id) => {
     return document.getElementById(id).classList.contains("active");
 }
 
-function makeActive(id) {
+const makeActive = (id) => {
     ["ar-exp-nav","silent-nav","alszol-nav"].forEach((item) => {
         elem = document.getElementById(item);
         if (id == item) {
-            elem.classList.add("active")
+            elem.classList.add("active");
         } else {
-            elem.classList.remove("active")
+            elem.classList.remove("active");
         }
     });
 }
